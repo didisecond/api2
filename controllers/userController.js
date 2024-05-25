a0706e2ec31bdfcbd9442ff4e3b9c25b7344285c
didisecond <d3131125@gmail.com>
2024-05-25 19:18:00 +0200

    ajstes para la verificacion y registro se haga en el fronted

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 
 // Obtener todos los usuarios
 export const getUsers = async (req, res) => {
@@ -15,34 +14,12 @@ export const getUsers = async (req, res) => {
 // Crear un nuevo usuario
 export const createUser = async (req, res) => {
     const { name, email, password } = req.body;
-    const user = new User({ name, email, password });
     try {
-        const newUser = await user.save();
-        res.status(201).json(newUser);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-};
-
-// Iniciar sesión de usuario
-export const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ email });
-        if (!user || !(await bcrypt.compare(password, user.password))) {
-            return res.status(400).json({ message: 'Correo electrónico o contraseña incorrectos' });
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'El usuario ya existe' });
         }
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.json({ token, user });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
 
-// Registrar un nuevo usuario
-export const registerUser = async (req, res) => {
-    const { name, email, password } = req.body;
-    try {
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         const newUser = await user.save();
@@ -51,4 +28,3 @@ export const registerUser = async (req, res) => {
         res.status(400).json({ message: err.message });
     }
 };
-
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import { getUsers, createUser, loginUser, registerUser } from '../controllers/userController.js';
+import { getUsers, createUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
 router.post('/', createUser);
-router.post('/login', loginUser);
-router.post('/register', registerUser);
 
 export default router;
